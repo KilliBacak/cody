7787f78c6f93ea9ab80db3ce501331bf8bd0cc51
KilliBacak <166852818+KilliBacak@users.noreply.github.com>
2024-04-12 19:53:35 +0300

    Add files via upload

---
+++ jeetkune.js
@@ -0,0 +1,13 @@
+        document.addEventListener("DOMContentLoaded", function() {
+            yall({
+                observeChanges: true
+            });
+        });
+		var pageEmbed = 0;
+		var pageRoom = 1;
+		var curPage = 'home';
+		var loadPage = '';
+		var bbfv = '?v=1.49';
+		var rtlMode = '0';
+		var logged = 0;
+		var utk = '0';
